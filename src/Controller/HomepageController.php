c711dcde4b74eee8209a57280151032e1d59e03f
Rayane <rayane.mallek@mynelis.com>
2023-03-12 11:02:03 +0100

    fix bug

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Concert;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
