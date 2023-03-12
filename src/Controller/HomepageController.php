623242e1ec6395a3ae52aaf4dc531b26d60110ea
Rayane <rayane.mallek@mynelis.com>
2023-03-12 10:58:09 +0100

    fix bug

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,19 +6,27 @@ use App\Entity\Concert;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/', name: 'app_')]
 class HomepageController extends AbstractController
 {
     #[Route('', name: 'homepage')]
-    public function index(ManagerRegistry $doctrine)
+    public function index(PaginatorInterface $paginator, Request $request, ManagerRegistry $doctrine)
     {
         $concertRepository = $doctrine->getRepository(Concert::class);
         $concerts = $concertRepository->findAll();
 
-        return $this->render('index.html.twig', [
-            'concerts' => $concerts,
-            'paginate' => true,
+        $pagination = $paginator->paginate(
+            $concerts,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('concerts/index.html.twig', [
+            'pagination' => $pagination,
         ]);
     }
 }
