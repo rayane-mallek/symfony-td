d25b5e8ed470b770a26f1a5a14f63b61e4ecc873
Rayane <rayane.mallek@mynelis.com>
2023-03-12 17:07:39 +0100

    fix bug

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/user')]
@@ -18,14 +19,20 @@ class UserController extends AbstractController
 {
 
     #[Route('/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, UserRepository $userRepository): Response
+    public function edit(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            var_dump($user->getPassword());exit;
+            $password = $user->getPassword();
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    new User(),
+                    $password
+                )
+            );
             $userRepository->save($user, true);
 
             return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
